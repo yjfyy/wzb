7941fbc36910b38d525d5b1ad17f6a4d6b3e11c0
yjfyy <yjfyeyu@gmail.com>
2018-03-31 11:46:34 +0800

    0.2

---
+++ 随机出题/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label_NO = New System.Windows.Forms.Label()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Button_run = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("宋体", 42.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(362, 286)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 56)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "第"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("宋体", 42.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(534, 286)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 56)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "题"
+        '
+        'Label_NO
+        '
+        Me.Label_NO.AutoSize = True
+        Me.Label_NO.Font = New System.Drawing.Font("宋体", 42.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label_NO.ForeColor = System.Drawing.Color.Red
+        Me.Label_NO.Location = New System.Drawing.Point(448, 286)
+        Me.Label_NO.Name = "Label_NO"
+        Me.Label_NO.Size = New System.Drawing.Size(52, 56)
+        Me.Label_NO.TabIndex = 2
+        Me.Label_NO.Text = "0"
+        '
+        'Button_run
+        '
+        Me.Button_run.Font = New System.Drawing.Font("宋体", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button_run.Location = New System.Drawing.Point(405, 368)
+        Me.Button_run.Name = "Button_run"
+        Me.Button_run.Size = New System.Drawing.Size(155, 50)
+        Me.Button_run.TabIndex = 3
+        Me.Button_run.Text = "开始"
+        Me.Button_run.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(905, 505)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "重新开始"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 662)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button_run)
+        Me.Controls.Add(Me.Label_NO)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "随机出题"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label_NO As Label
+    Friend WithEvents Button_run As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ 随机出题/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Dim no
+    Dim biaohao = 0
+    Private Sub Button_run_Click(sender As Object, e As EventArgs) Handles Button_run.Click
+
+        Label_NO.Text = no(biaohao)
+        If biaohao = 35 Then
+            MsgBox("题以出完")
+        Else
+            biaohao = biaohao + 1
+        End If
+
+    End Sub
+    Public Function MakeRndSum(ByVal iMin As Integer, ByVal iMax As Integer, ByVal iSum As Integer) As Integer()
+        '**************************************
+        '功能:随机生成iMin到iMax之间的iSum个数,将结果作为数组返回给调用程序
+        '参数:iMin,最小取值,没有则无限制
+        '     iMax,最大取值,没有则无限制
+        '     iSum,需要生成的随机数的个数
+        '返回值:调用成功,返回一个数组,且数组大小不为0
+        '       调用失败,返回一个大小为零的数组
+        '**************************************
+
+        Dim iI, iJ, iNum, iTemp As Integer
+        Dim aiSult() As Integer
+
+        ReDim aiSult(iSum)
+
+        iNum = 0
+        For iI = 0 To iSum - 1
+
+Line1:
+            Randomize()
+            iTemp = CInt(iMin + Rnd() * (iMax - iMin))
+
+            '判断是否有重复的数据
+            For iJ = 0 To iI - 1
+                If iTemp = aiSult(iJ) Then
+                    GoTo Line1
+                End If
+            Next
+
+            '保存数据
+            aiSult(iNum) = iTemp
+            iNum = iNum + 1
+
+        Next
+
+        Return aiSult
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        no = MakeRndSum(1, 35, 35)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MsgBox("重新开始", vbQuestion + vbYesNo) = vbYes Then
+            no = MakeRndSum(1, 35, 35)
+            biaohao = 0
+        Else
+
+        End If
+    End Sub
+End Class
